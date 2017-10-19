386e895a5b7c8a03629ee1cdbeea86157a17d8b9
zachlefevre <zachlefevre@gmail.com>
2017-10-19 14:23:59 -0500

    added tests for new ll functionality

--- chapter 2/ll/ll_test.go
+++ chapter 2/ll/ll_test.go
@@ -1,6 +1,7 @@
 package ll
 
 import "testing"
+import "fmt"
 
 func TestAppending(t *testing.T) {
 	var list Sll
@@ -12,5 +13,39 @@ func TestAppending(t *testing.T) {
 	}
 
 	list.Printll()
+}
 
+func TestFind(t *testing.T) {
+	var list Sll
+	list.Append(&Node{Data: 88})
+	list.Append(&Node{Data: 89})
+	list.Append(&Node{Data: 11})
+	for i := 0; i <= 22; i++ {
+		list.Append(&Node{Data: i})
+	}
+
+	prev, d := list.Find(4)
+	fmt.Println(prev, d)
+
+	prev, d = list.Find(88)
+	fmt.Println(prev, d)
+
+	prev, d = list.Find(22)
+	fmt.Println(prev, d)
+
+	prev, d = list.Find(188)
+	fmt.Println(prev, d)
+}
+
+func TestToSlice(t *testing.T) {
+	var list Sll
+	list.Append(&Node{Data: 88})
+	list.Append(&Node{Data: 89})
+	list.Append(&Node{Data: 11})
+	for i := 0; i <= 22; i++ {
+		list.Append(&Node{Data: i})
+	}
+
+	s := list.Lltoas()
+	fmt.Println(s)
 }
