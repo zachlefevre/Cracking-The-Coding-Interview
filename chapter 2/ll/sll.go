2e62638219f6a4abe69e38b301d299bdba400c15
zachlefevre <zachlefevre@gmail.com>
2017-10-20 10:02:35 -0500

    Made everything exported

--- chapter 2/ll/sll.go
+++ chapter 2/ll/sll.go
@@ -6,21 +6,21 @@ import (
 
 type Node struct {
 	Data int
-	next *Node
+	Next *Node
 }
 
 type Sll struct {
-	head *Node
+	Head *Node
 }
 
 func (ll *Sll) Append(n *Node) bool {
-	if ll.head == nil {
-		ll.head = n
+	if ll.Head == nil {
+		ll.Head = n
 	} else {
-		trav := ll.head
-		for ; trav.next != nil; trav = trav.next {
+		trav := ll.Head
+		for ; trav.Next != nil; trav = trav.Next {
 		}
-		trav.next = n
+		trav.Next = n
 		return true
 	}
 	return false
@@ -30,19 +30,19 @@ func (ll *Sll) Append(n *Node) bool {
 Return the node before the node containing the searched data, and the node itself
 */
 func (ll *Sll) Find(d int) (*Node, *Node) {
-	if ll.head.Data == d {
-		//If the head is found with the searched data, return nil and the head.
-		return nil, ll.head
+	if ll.Head.Data == d {
+		//If the Head is found with the searched data, return nil and the Head.
+		return nil, ll.Head
 	}
-	for trav := ll.head; trav != nil; trav = trav.next {
+	for trav := ll.Head; trav != nil; trav = trav.Next {
 		//Traverse the list, and if the node is found, return the previous node and the searched node.
-		if trav.next == nil {
+		if trav.Next == nil {
 			//If trav is the tail of the list, it has already been checked and is not the searched node
 			//return nil nil
 			return nil, nil
 		}
-		if trav.next.Data == d {
-			return trav, trav.next
+		if trav.Next.Data == d {
+			return trav, trav.Next
 		}
 	}
 	return nil, nil
@@ -54,22 +54,22 @@ func (ll *Sll) Remove(d int) bool {
 		return false
 	}
 	if prev == nil {
-		ll.head = ll.head.next
+		ll.Head = ll.Head.Next
 		return true
 	}
-	prev.next = toRemove.next
+	prev.Next = toRemove.Next
 	return true
 }
 
 func (ll *Sll) Printll() {
-	for trav := ll.head; trav != nil; trav = trav.next {
+	for trav := ll.Head; trav != nil; trav = trav.Next {
 		fmt.Print(trav.Data, " -> ")
 	}
 	fmt.Println("//")
 }
 
 func (ll *Sll) Lltoas() (s []int) {
-	for trav := ll.head; trav != nil; trav = trav.next {
+	for trav := ll.Head; trav != nil; trav = trav.Next {
 		s = append(s, trav.Data)
 	}
 	return
