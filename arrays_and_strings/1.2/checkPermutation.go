2fe7d6184b8a191a3291dbd1e6c7fc5061a4e085
zachary lefevre <zachlefevre@gmail.com>
2018-12-30 19:27:21 -0600

    Implemented 1.2 using a map

--- arrays_and_strings/1.2/checkPermutation.go
+++ arrays_and_strings/1.2/checkPermutation.go
@@ -49,12 +49,31 @@ func isPermutation_Sort(s1, s2 s) bool {
 	return false
 }
 
+func isPermutation_Map(s1, s2 s) bool {
+	if len(s1) != len(s2) {
+		return false
+	}
+
+	lookup := make(map[int]int)
+
+	for _, el := range s1 {
+		lookup[el] = lookup[el] + 1
+	}
+	for _, el := range s2 {
+		lookup[el] = lookup[el] - 1
+		if lookup[el] < 0 {
+			return false
+		}
+	}
+	return true
+}
+
 func main() {
-	str1 := s{1, 7, 53, 6, 8, 88, 9, 64, 5}
-	str2 := s{1, 7, 53, 88, 6, 8, 9, 64, 5}
+	str1 := s{1, 7, 53, 6, 8, 88, 9, 64, 5, 1}
+	str2 := s{1, 7, 53, 88, 6, 8, 9, 64, 5, 2}
 	fmt.Println("first string: ", str1)
 	fmt.Println("second string: ", str2)
-	if isPermutation_Sort(str1, str2) {
+	if isPermutation_Map(str1, str2) {
 		fmt.Println("permutation")
 	} else {
 		fmt.Println("no permutation")
