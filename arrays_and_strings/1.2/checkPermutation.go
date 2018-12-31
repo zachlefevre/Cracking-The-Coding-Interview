d07cf85b91732dc3f410fe4ea0862215722331f8
zachary lefevre <zachlefevre@gmail.com>
2018-12-30 19:19:07 -0600

    cleanup

--- arrays_and_strings/1.2/checkPermutation.go
+++ arrays_and_strings/1.2/checkPermutation.go
@@ -37,19 +37,26 @@ func (str s) Swap(a, b int) {
 	return
 }
 
+func isPermutation_Sort(s1, s2 s) bool {
+	if len(s1) != len(s2) {
+		return false
+	}
+	sort.Sort(s1)
+	sort.Sort(s2)
+	if reflect.DeepEqual(s1, s2) {
+		return true
+	}
+	return false
+}
+
 func main() {
 	str1 := s{1, 7, 53, 6, 8, 88, 9, 64, 5}
 	str2 := s{1, 7, 53, 88, 6, 8, 9, 64, 5}
 	fmt.Println("first string: ", str1)
 	fmt.Println("second string: ", str2)
-	if len(str1) != len(str2) {
-		fmt.Println("Not permutations of one another. Length bad")
-	}
-	sort.Sort(str1)
-	sort.Sort(str2)
-	if reflect.DeepEqual(str1, str2) {
-		fmt.Println("Permutations of one another")
+	if isPermutation_Sort(str1, str2) {
+		fmt.Println("permutation")
 	} else {
-		fmt.Println("Not permutations of one another")
+		fmt.Println("no permutation")
 	}
 }
